d3073577331a13762d8bf29ab568c311365ac0b8
Yulia <heaven1209@yandex.ru>
2023-02-26 16:07:30 +0300

    Change the language menu

--- ProjectChat/Chat.cpp
+++ ProjectChat/Chat.cpp
@@ -20,14 +20,14 @@ const std::shared_ptr<User> Chat::getCurrentUser()
 void Chat::signUp() // Регистрация пользователя
 {
 	std::string name, login, password;
-	std::cout << "Введите Ваше имя" << std::endl;
+	std::cout << "Enter your Name" << std::endl;
 	std::cin >> name;
-	std::cout << "Введите логин" << std::endl;
+	std::cout << "Enter login" << std::endl;
 	std::cin >> login;
 	try
 	{
 		checkLogin(login);
-		std::cout << "Введите пароль" << std::endl;
+		std::cout << "Enter password" << std::endl;
 		std::cin >> password;
 		_users.emplace_back(name, login, password);
 	}
@@ -52,9 +52,9 @@ void Chat::addMessage(const std::string& login) // Отправка сообщения
 {
 	std::string from, to, text;
 	from = login;
-	std::cout << "Введите логин пльзователя или all для отправки всем участникам" << std::endl;
+	std::cout << "Enter user name or 'all' for send all users" << std::endl;
 	std::cin >> to;
-	std::cout << "Введите текст сообщения" << std::endl;
+	std::cout << "Enter message" << std::endl;
 	std::cin.ignore();
 	getline(std::cin, text);
 	_messages.emplace_back(from, to, text);
@@ -66,7 +66,7 @@ void Chat::showMessage(const std::string& login) // Показать входящие сообщения
 	{
 		if (login == message.getTo() || message.getTo() == "all")
 		{
-			std::cout << "Пользователь " << message.getFrom() << " написал:" << std::endl
+			std::cout << "User " << message.getFrom() << " written:" << std::endl
 				<< message.getText() << std::endl;
 		} 
 	}
@@ -74,7 +74,7 @@ void Chat::showMessage(const std::string& login) // Показать входящие сообщения
 
 void Chat::showUsers() // Показать зарегистрированных пользователей
 {
-	std::cout << "Пользователи в системе:" << std::endl;
+	std::cout << "Users in system:" << std::endl;
 	for (size_t i = 0; i < _users.size(); i++)
 	{
 		std::cout << _users[i].getName() << '\n';
@@ -87,7 +87,7 @@ bool Chat::checkUser(const std::string& login, const std::string& password) // П
 	{
 		if (login == user.getLogin() && password == user.getPassword())
 		{
-			std::cout <<"Вы вошли под именем: " << user.getName() << std::endl;
+			std::cout <<"Welcome: " << user.getName() << std::endl;
 			_currentUser = std::make_shared<User>(user);
 			return true;
 		}
@@ -98,13 +98,13 @@ void Chat::signIn() // Вход пользователя
 {
 	bool check = false;
 	std::string login, password;
-	std::cout << "Введите логин" << std::endl;
+	std::cout << "Enter your login" << std::endl;
 	std::cin >> login;
-	std::cout << "Введите пароль" << std::endl;
+	std::cout << "Enter password" << std::endl;
 	std::cin >> password;
 	if (!checkUser(login, password))
 	{
-		std::cout << "Пароль или логин неверный" << std::endl;
+		std::cout << "Login or passwor error" << std::endl;
 	}
 	else
 	{
@@ -116,7 +116,7 @@ void Chat::showStartMenu() //Стартовое меню
 {
 	char menu = '0';
 	do {
-		std::cout << "Введите 1 - для регистрации, 2 - для входа, 0 - для выхода из программы" << std::endl;
+		std::cout << "Enter 1 - registration, 2 - sign in, 0 - exit" << std::endl;
 		std::cin >> menu;
 		switch (menu)
 		{
@@ -130,7 +130,7 @@ void Chat::showStartMenu() //Стартовое меню
 			_chatWork = false;
 			break;
 		default:
-			std::cout << "Ошибка, введите корректный символ из меню" << std::endl;
+			std::cout << "Error, enter the correct number of menu" << std::endl;
 			break;
 		}
 	} while (!_currentUser && _chatWork);
@@ -140,7 +140,7 @@ void Chat::showUserMenu() //Меню залогиненного пользователя
 {
 	char menu = '0';
 	while (_currentUser) {
-		std::cout << "Введите 1 - Входящие, 2 - Отправить сообщение, 3 - Список пользователей, 4 - Выход из аккаунта, 0 - Выход из программы" 
+		std::cout << "Enter 1 - incoming message, 2 - Send message, 3 - Users, 4 - Log out, 0 - Exit programm" 
 			<< std::endl;
 		std::cin >> menu;
 		switch (menu) {
@@ -161,7 +161,7 @@ void Chat::showUserMenu() //Меню залогиненного пользователя
 			_chatWork = false;
 			break;
 		default:
-			std::cout << "Ошибка, введите корректный символ из меню" << std::endl;
+			std::cout << "Error, enter the correct number of menu" << std::endl;
 			break;
 		}
 	}
@@ -176,7 +176,7 @@ void Chat::saveUsers() // Сохранение пользователей в файл
 		}
 	}
 	else {
-		std::cout << "Невозможно открыть файл!" << '\n';
+		std::cout << "Could not open the file!" << '\n';
 	}
 	outfile.close();
 }
@@ -190,7 +190,7 @@ void Chat::saveMessages() // Сохранение сообщений в файл
 		}
 	}
 	else {
-		std::cout << "Невозможно открыть файл!" << '\n';
+		std::cout << "Could not open the file!" << '\n';
 	}
 	outfile.close();
 }
@@ -206,7 +206,7 @@ void Chat::loadUsers()
 		}
 	}
 	else {
-		std::cout << "Невозможно открыть файл!" << '\n';
+		std::cout << "Could not open the file!" << '\n';
 	}
 	inputfile.close();
 }
@@ -222,7 +222,7 @@ void Chat::loadMessages()
 		}
 	}
 	else {
-		std::cout << "Невозможно открыть файл!" << '\n';
+		std::cout << "Could not open the file!" << '\n';
 	}
 	inputfile.close();
 }
